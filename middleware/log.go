e893b43a4526be8e7a4875895a32f78b6c976b5e
Waldo <waldofelix1@gmail.com>
2021-09-22 23:01:44 +0700

    better log

--- middleware/log.go
+++ middleware/log.go
@@ -7,8 +7,9 @@ import (
 
 func LogMiddlewareInit(e *echo.Echo) {
 	logger := middleware.LoggerConfig{
-		Format: `{"time":"${time_rfc3339_nano}","level":"${level}","prefix":"${prefix}","file":"${short_file}","line":"${line}"}
-		` + "\n",
+		// Format: `{"time":"${time_rfc3339_nano}","level":"${level}","prefix":"${prefix}","file":"${short_file}","line":"${line}"}
+		// ` + "\n",
+		Format: `[${time_rfc3339}] ${status} ${method} ${host} ${path} ${latency_human}` + "\n",
 	}
 
 	e.Use(middleware.LoggerWithConfig(logger))
